f13ad592d1811028b70e1b16f7b8294129dc46f4
adburke <adburke@fullsail.edu>
2013-10-31 02:25:40 -0500

    Finished game logic and displaying results in a single textview color coded

--- Java1-W1/Java1_p1Project/Java1_p1/src/main/java/com/adburke/java1_p1/Java1_p1.java
+++ Java1-W1/Java1_p1Project/Java1_p1/src/main/java/com/adburke/java1_p1/Java1_p1.java
@@ -5,10 +5,13 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.graphics.Color;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.text.InputFilter;
+import android.text.Spannable;
+import android.text.style.ForegroundColorSpan;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -31,7 +34,7 @@ public class Java1_p1 extends Activity {
     // Testing out logging
     final String TAG = "GuessGame";
 
-    // Holds results of tries
+    // Holds results of tries in a 2D List format: [[guess,actual,win/loss(0 or 1)],[guess,actual,win/loss(0 or 1)]]
     final List<List<Integer>> gameResults = new ArrayList<List<Integer>>();
 
     @Override
@@ -78,19 +81,33 @@ public class Java1_p1 extends Activity {
         btn.setText(R.string.btn_text);
         btn.setEnabled(false);
         projectLayout.addView(btn);
+
+        // Game results TextView
+        // Game results textView which needs to be modified by displayResults()
+        final TextView resultsText = new TextView(this);
+        lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        resultsText.setLayoutParams(lp);
+        resultsText.setText(R.string.result_header);
+        resultsText.setSingleLine(false);
+        projectLayout.addView(resultsText);
+
+        // Button onClick event
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int val = Integer.parseInt(inputTxt.getText().toString());
                 boolean result = checkInput(val);
 
+                // Check return value of comparison and set msgText accordingly
                 if (result) {
                     msgText.setText(messages[4]);
 
                 } else {
                     msgText.setText(messages[5]);
-                    
+
                 }
+                // Show results below button
+                displayResults(resultsText);
             }
         });
 
@@ -136,11 +153,6 @@ public class Java1_p1 extends Activity {
         });
 
 
-
-        
-
-
-
         setContentView(projectLayout);
     }
 
@@ -153,21 +165,46 @@ public class Java1_p1 extends Activity {
         int actualNum = randomNumber.nextInt((10 - 1) + 1) + 1;
         Log.i(TAG, "actual = " + actualNum);
 
-        Log.i(TAG, "gameResults rows = " + gameResults.size());
-
         // Check the arrayList size to increment the 2D row with new guess and actual values
+        Log.i(TAG, "gameResults rows = " + gameResults.size());
         int size = gameResults.size();
-
         gameResults.add(new ArrayList<Integer>());
         gameResults.get(size).add(x);
         gameResults.get(size).add(actualNum);
-
         if (x == actualNum) {
-            return true;
+            gameResults.get(size).add(1);
         } else {
-            return false;
+            gameResults.get(size).add(0);
         }
 
+
+        // Simplified if else for the return
+        return (x == actualNum);
+
+    }
+
+    // Method to display results
+    public void displayResults(TextView txtView) {
+        int size = gameResults.size();
+
+        for (int i = size - 1, j = size; i < j; i++) {
+            String newstring =  " \n" + "Guess: " + gameResults.get(i).get(0) + " Actual: " + gameResults.get(i).get(1);
+            if (gameResults.get(i).get(2) == 0) {
+                appendColoredText(txtView, newstring, Color.RED);
+            } else {
+                appendColoredText(txtView, newstring, Color.GREEN);
+            }
+        }
+    }
+
+    // Method to color results
+    public static void appendColoredText(TextView txtView, String text, int color) {
+        int start = txtView.getText().length();
+        txtView.append(text);
+        int end = txtView.getText().length();
+
+        Spannable spannableText = (Spannable) txtView.getText();
+        spannableText.setSpan(new ForegroundColorSpan(color), start, end, 0);
     }
 
 }
