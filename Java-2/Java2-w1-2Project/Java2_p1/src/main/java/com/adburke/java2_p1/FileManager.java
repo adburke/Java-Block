22587e0c37c2a61a99f42fc5a004a1dd19af35ba
adburke <adburke@fullsail.edu>
2013-12-03 17:19:58 -0600

    FileManager singleton setup and wire frame for write and read methods

--- Java-2/Java2-w1-2Project/Java2_p1/src/main/java/com/adburke/java2_p1/FileManager.java
+++ Java-2/Java2-w1-2Project/Java2_p1/src/main/java/com/adburke/java2_p1/FileManager.java
@@ -10,5 +10,41 @@
 
 package com.adburke.java2_p1;
 
+import android.content.Context;
+
+import org.json.JSONObject;
+
 public class FileManager {
+
+    // Singleton Creation
+    private static FileManager m_instance;
+    private FileManager(){
+        // Constructor empty for singleton
+    }
+    // Check if m_instance is null, if so create the singleton, otherwise it is created already
+    public static FileManager getMinstance() {
+        if (m_instance == null) {
+            m_instance = new FileManager();
+        }
+        return m_instance;
+    }
+
+
+    public boolean WriteFile(Context context, String filename, String content) {
+        boolean result = false;
+
+
+
+        return result;
+    }
+
+    public String ReadFile() {
+        String result = null;
+
+
+        
+        return result;
+    }
+
+
 }
