0326f5b097512c62d9d10b4b8c785f38c4df3de8
adburke <adburke@fullsail.edu>
2013-12-11 21:14:16 -0600

    Work on ProductListDetail activity data

--- Java-2/Java2-w3-Project/Java2_p1/src/main/java/com/adburke/java2_p1/ProductListDetail.java
+++ Java-2/Java2-w3-Project/Java2_p1/src/main/java/com/adburke/java2_p1/ProductListDetail.java
@@ -12,21 +12,29 @@ package com.adburke.java2_p1;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Toast;
 
 import java.io.Serializable;
+import java.net.URI;
+import java.util.HashMap;
 
 public class ProductListDetail extends ActionBarActivity {
     static Context mContext;
+    int productIndex;
+    HashMap<String, String> productInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,48 @@ public class ProductListDetail extends ActionBarActivity {
 
         // Initialize context
         mContext = this;
+
+        // URI for ContentProvider
+        Uri productUri = null;
+
+        // Capture incoming data
+        Bundle incomingData = getIntent().getExtras();
+        if (incomingData != null) {
+            productIndex = incomingData.getInt("index");
+            //Toast.makeText(mContext, "Product Index: " + productIndex, Toast.LENGTH_LONG).show();
+            // Create URI for product call to capture data
+            productUri = Uri.parse("content://" + CollectionProvider.AUTHORITY + "/items/" + productIndex);
+        }
+
+        if (productUri != null) {
+            // Call the ContentProvider with the relevant URI
+            Cursor cursor = getContentResolver().query(productUri, null, null, null, null);
+
+            // Check if the cursor returned values
+            if (cursor == null) {
+                Toast.makeText(this, "Cursor value is null", Toast.LENGTH_LONG).show();
+                Log.i("onListUpdate", "NULL CURSOR AT: " + productUri.toString());
+            }
+            // Clear the product list of values if already created
+            if (productInfo != null) {
+                productInfo.clear();
+            }
+
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    for (int i = 0, j = cursor.getCount(); i < j; i++) {
+                        HashMap<String, String> productInfo = new HashMap<String, String>();
+
+                        productInfo.put("productName", cursor.getString(1));
+                        productInfo.put("vendor", cursor.getString(2));
+                        productInfo.put("productPrice", cursor.getString(3));
+
+                    }
+                }
+            }
+        }
+
+
 
     }
 
