bded03aa00ef2014e57dee12fb7a4b5605f934a2
adburke <adburke@fullsail.edu>
2013-12-16 19:45:07 -0600

    Created layout fragments for activities

---
+++ Java-2/Java2-w4-Project/Java2_p1/src/main/java/com/adburke/java2_p1/ProductDetailFragment.java
@@ -0,0 +1,21 @@
+/*
+ * Project:		Java2-w4-Project
+ *
+ * Package:		Java2_p1
+ *
+ * Author:		aaronburke
+ *
+ * Date:		 	12 16, 2013
+ */
+
+package com.adburke.java2_p1;
+
+
+import android.app.Fragment;
+
+/**
+ * Created by aaronburke on 12/16/13.
+ */
+public class ProductDetailFragment extends Fragment {
+
+}
